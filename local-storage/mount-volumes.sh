b2a41151ef0f9e03c802a29bb80ef91ae4a9ae11
Tosin Akinosho <takinosh@redhat.com>
2021-07-23 15:40:12 -0400

    updating permissions for mount volume

--- local-storage/mount-volumes.sh
+++ local-storage/mount-volumes.sh
@@ -17,7 +17,8 @@ do
     ${SUDO} mkfs.ext4 ${disk} 2>/dev/null
     #create dirctory
     ${SUDO} mkdir -p /data/local-pvc-${disk_name}
-    ${SUDO}  /data/local-pvc-${disk_name}
+    ${SUDO} chown $USER:$USER /data/local-pvc-${disk_name}
+    ${SUDO}  chmod 777 /data/local-pvc-${disk_name}
     #mount for start up
 ${SUDO} tee -a /etc/fstab > /dev/null <<EOT
 ${disk} /data/local-pvc-${disk_name} auto noatime,noexec,nodiratime 0 0
